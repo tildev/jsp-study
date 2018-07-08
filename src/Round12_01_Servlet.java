80fa17ae8b55ff3eb34620e1a31e7e9aaad05961
tildev <tildevelove@gmail.com>
2018-07-08 20:05:27 +0900

    서블릿 작동 및 web.xml 설정

---
+++ src/Round12_01_Servlet.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * @author tildev
+ * @date 2018. 7. 8.
+ */
+public class Round12_01_Servlet extends GenericServlet{
+
+	@Override
+	public void init() {
+		System.out.println("서블릿 파일 초기화 부분!");
+	}
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		System.out.println("서블릿 내용 부분");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("서블릿 파일 데이터 소멸 부분!");
+	}
+
+	
+}
