5b5882c141ee543f36a0130e1fcacedecce79ee0
tildev <tildevelove@gmail.com>
2018-07-10 11:54:04 +0900

    servlet 실습 3 진행

---
+++ src/Round12_03_Servlet.java
@@ -0,0 +1,69 @@
+
+/**
+ * 
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author tildev
+ * @date 2018. 7. 8.
+ */
+public class Round12_03_Servlet extends HttpServlet {
+
+	@Override
+	public void init() {
+		System.out.println("서블릿 파일 초기화 부분!");
+	}
+
+	@Override
+	protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Head Request.");
+		out.close();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Get Request.");
+		out.close();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Post Request.");
+		out.close();
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Put Request.");
+		out.close();
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("DeleteO Request.");
+		out.close();
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("서블릿 파일 데이터 소멸 부분!");
+		// 서블릿 로그 파일에 메시지를 남긴다.
+		log("서블릿 관련 데이터 소멸!");
+	}
+
+}
